c0c00834983c7ba847b2ee8df2dd7572e99401f0
CrystalBow <117384707+CrystalBow@users.noreply.github.com>
2024-02-08 18:04:45 -0500

    Text

--- lab_assignment_4.c
+++ lab_assignment_4.c
@@ -9,6 +9,14 @@ typedef struct node {
 // Returns number of nodes in the linkedList.
 int length(node* head)
 {
+    int counter = 0;
+    if (head == NULL) {
+        return counter;
+    }
+    while (head->next != NULL) {
+        head = head->next;
+        counter++;
+    }
 }
 
 // parses the string in the linkedList
@@ -16,6 +24,17 @@ int length(node* head)
 //  then toCString function wil return "abc"
 char* toCString(node* head)
 {
+    int l = length(head), count=0;
+    char stringy[length(head) + 1];
+    stringy[l] = '\0';
+    if (head == NULL) {
+        return stringy;
+    }
+    for (count = 0; count < l; count++) {
+        stringy[count] = head->letter;
+        head = head->next;
+    }
+    return stringy;
 }
 
 // inserts character to the linkedlist
@@ -24,6 +43,19 @@ char* toCString(node* head)
 // head -> |a|->|b|->|c|->|x|
 void insertChar(node** pHead, char c)
 {
+    node* new = (node*) malloc(sizeof(node));
+    new->letter = c;
+    node* current = *pHead;
+    if (current == NULL) {
+        *pHead = new;
+        printf("%c\n", new->letter);
+    } else {
+        while (current->next != NULL){
+            current = current->next;
+        }
+        current->next = new;
+        printf("%c\n", new->letter);
+    }
 }
 
 // deletes all nodes in the linkedList.
